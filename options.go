f3fc6a0078412b2f5dc9f4aab9e1d457fcb160da
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-09-22 13:35:55 +0300

    update to latest micro
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- options.go
+++ options.go
@@ -23,20 +23,20 @@ type netListener struct{}
 // send.  Default maximum message size is 8K
 //
 func MaxMsgSize(s int) server.Option {
-	return setServerOption(maxMsgSizeKey{}, s)
+	return server.SetOption(maxMsgSizeKey{}, s)
 }
 
 // AuthTLS should be used to setup a secure authentication using TLS
 func AuthTLS(t *tls.Config) server.Option {
-	return setServerOption(tlsAuth{}, t)
+	return server.SetOption(tlsAuth{}, t)
 }
 
 // MaxConn specifies maximum number of max simultaneous connections to server
 func MaxConn(n int) server.Option {
-	return setServerOption(maxConnKey{}, n)
+	return server.SetOption(maxConnKey{}, n)
 }
 
 // Listener specifies the net.Listener to use instead of the default
 func Listener(l net.Listener) server.Option {
-	return setServerOption(netListener{}, l)
+	return server.SetOption(netListener{}, l)
 }
